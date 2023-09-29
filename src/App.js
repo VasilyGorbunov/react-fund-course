fd0a35a2d7344a5c4d4ee43adf46bc80feb64ca3
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-09-29 18:29:20 +0300

    46:55

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/app.css';
+
+import {useState} from "react";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Vue JS', body: 'Description'},
+        {id: 3, title: 'ReactJS', body: 'Description'}
+    ]);
+
+    return (
+        <div className="App">
+            <form style={{marginTop: '14px'}}>
+                <input type="text" placeholder="Title"/>
+                <input type="text" placeholder="Description"/>
+                <MyButton>Create Post</MyButton>
+            </form>
+            <PostList posts={posts} title="Список постов"/>
+        </div>
+    );
 }
 
 export default App;
